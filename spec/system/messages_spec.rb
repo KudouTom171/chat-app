7cd604aa00b299a8fbb4a9aa31a29f477efe79c7
KudouTom171 <76691187+KudouTom171@users.noreply.github.com>
2021-01-17 19:28:55 +0900

    結合テストの実装　修正

--- spec/system/messages_spec.rb
+++ spec/system/messages_spec.rb
@@ -1,9 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe "メッセージ投稿機能", type: :system do
+RSpec.describe "Messages", type: :system do
   before do
-    # 中間テーブルを作成して、usersテーブルとroomsテーブルのレコードを作成する
-    @room_user = FactoryBot.create(:room_user)
+    driven_by(:rack_test)
   end
 
   context '投稿に失敗したとき' do
